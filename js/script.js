eccf2d8e26af1f3a5ff133c05442d241b43ceff9
Jmukumzoda <jmukumzoda63@gmail.com>
2023-08-21 16:09:29 +0500

    Wepro

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function min(a, b) {
+    for(let i =0; i>min; i++){
+
+        if (a < b) {
+            console.log(a);
+        }
+    }
+     typeof a
+    console.log(a, b);
+}
+min(1, 2)
+min([1, 2])
