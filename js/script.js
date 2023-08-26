2fae802f0c877735beb73eaa659eee82113bbf74
Jmukumzoda <jmukumzoda63@gmail.com>
2023-08-26 20:38:13 +0500

    bg_menu update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,38 @@
-function min(a, b) {
-    for(let i =0; i>min; i++){
+// function min(a, b) {
+//     for(let i =0; i>min; i++){
 
-        if (a < b) {
-            console.log(a);
-        }
+//         if (a < b) {
+//             console.log(a);
+//         }
+//     }
+//      typeof a
+//     console.log(a, b);
+// }
+// min(1, 2)
+// min([1, 2])
+
+let menu = document.querySelector('.menu')
+let asside = document.querySelector('aside')
+let ul_li = document.querySelector('.ul_li2')
+let bg_menu = document.querySelector('.b_menu')
+let exitBtn = document.querySelector('.exit-btn')
+
+menu.onclick = () => {
+    if (asside.style.right === '0%') {
+        asside.style.right = '-100%'
+    }else{
+        asside.style.right = '0%'
     }
-     typeof a
-    console.log(a, b);
 }
-min(1, 2)
-min([1, 2])
+exitBtn.onclick = () => {
+    asside.style.right = '-100%'
+}
+
+
+// open.onclick = () => {
+//     if (asside.style.right === '0%') {
+//         asside.style.right = '-100%'
+//     } else {
+//         asside.style.right = '0%'
+//     }
+// }
